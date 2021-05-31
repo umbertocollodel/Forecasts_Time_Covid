0a1c61adcb99ebdd707914a4f0bae02c114e3e71
Umberto Collodel <umberto.collodel@gmail.com>
2021-05-31 11:00:38 +0100

    finished plotting; add actual line, check consistency and clean code

--- 2.3 comparison_weo_consensus_income_group.R
+++ 2.3 comparison_weo_consensus_income_group.R
@@ -7,45 +7,44 @@ list_weights=c("adv","em","lidc") %>%
 # Prepare weights dataframe for merging with comparison dataframe (need to have all horizons) -----
 # Note: apply same weights for following months after WEO issue before the next issue
 
-weights_global_comparison <- rea %>% 
-  split(.$horizon) %>% 
-  rep(c(2,3,4,3)) %>% 
-  map2(c("Apr","May","Jan","Feb","Mar","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
-       ~ .x %>% mutate(horizon = .y)) %>% 
-  bind_rows()
+
+extrapolate_horizons <- function(df){
+  df %>% 
+    split(.$horizon) %>% 
+    rep(c(2,3,4,3)) %>% 
+    map2(c("Apr","May","Jan","Feb","Mar","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
+         ~ .x %>% mutate(horizon = .y)) %>% 
+    bind_rows()
+}
 
 
+comparison_list <- list_weights %>% 
+  map(~ extrapolate_horizons(.x)) %>% 
+  map(~ .x %>% merge(read_xlsx("../Forecasts_Time_Covid_material/intermediate_data/replication_figures/comparison_individual_countries.xlsx") %>% 
+        rename(ifscode = country_code), by=c("ifscode","horizon"))) %>% 
+  map(~ .x %>% as_tibble()) %>% 
+  map(~ .x %>% mutate(imf_weighted = imf*weight,
+         consensus_weighted = consensus*weight )) %>% 
+  map(~ .x %>% group_by(horizon)) %>% 
+  map(~ .x %>% summarise(group_consensus = sum(consensus_weighted,na.rm = T),
+            group_imf = sum(imf_weighted, na.rm = T))) %>% 
+  map(~ .x %>% mutate(group_imf = case_when(group_imf == 0 ~ NA_real_,
+                                T ~ group_imf))) %>% 
+  map(~ .x %>% mutate(horizon = factor(horizon, levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
+                                              "Sep","Oct","Nov","Dec")))) %>% 
+  map(~ .x %>% gather("institution","value",group_consensus:group_imf)) %>% 
+  map(~ .x %>% mutate(institution = case_when(institution == "group_consensus" ~ "Consensus",
+                                 institution == "group_imf" ~ "WEO"))) 
 
-# Combine comparison dataframe with weight dataframes: -----
 
-
-global_comparison_df <- read_xlsx("../Forecasts_Time_Covid_material/intermediate_data/replication_figures/comparison_individual_countries.xlsx") %>% 
-  merge(readRDS("../Forecasts_Time_Covid_material/intermediate_data/weights_global.RDS"), by=c("country_code","horizon")) %>% 
-  merge(readRDS("../Forecasts_Time_Covid_material/intermediate_data/weights_global_actual.RDS"), by=c("country_code")) %>% 
-  as_tibble() %>%
-  mutate(imf_weighted = imf*weight,
-         consensus_weighted = consensus*weight,
-         actual_weighted = actual*actual_weight) %>% 
-  group_by(horizon) %>% 
-  summarise(global_consensus = sum(consensus_weighted,na.rm = T),
-            global_imf = sum(imf_weighted, na.rm = T),
-            global_actual = sum(actual_weighted, na.rm = T)) %>% 
-  mutate(global_imf = case_when(global_imf == 0 ~ NA_real_,
-                                T ~ global_imf)) %>% 
-  mutate(horizon = factor(horizon, levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
-                                              "Sep","Oct","Nov","Dec"))) %>% 
-  gather("institution","value",global_consensus:global_imf) %>% 
-  mutate(institution = case_when(institution == "global_consensus" ~ "Consensus",
-                                 institution == "global_imf" ~ "WEO")) 
-
+  
 
 
 # Plot and export (both plot and df for construction): -----
 
-global_comparison_df %>% 
-  ggplot(aes(horizon,value, col = institution)) +
+comparison_list %>% 
+  map(~ .x %>% ggplot(aes(horizon,value, col = institution)) +
   geom_vline(xintercept = c("Jan","Apr","Jun","Oct"), size = 15, col = "gray", alpha = 0.7) + 
-  geom_hline(aes(yintercept = global_actual, linetype = "Actual"), size = 1.5) +
   geom_point(size = 3, alpha = 0.8) +
   ylab("Real GDP Growth Forecast (%)") +
   xlab("") +
@@ -56,7 +55,7 @@ global_comparison_df %>%
   theme(legend.position = "bottom",
         legend.text = element_text(size = 15)) +
   theme(axis.text = element_text(size = 18),
-        axis.title = element_text(size = 21))
+        axis.title = element_text(size = 21)))
 
 
 
@@ -65,3 +64,4 @@ ggsave("../Forecasts_Time_Covid_material/output/figures/aggregate_comparison/Glo
        width = 12.3)
 
 export(global_comparison_df, "../Forecasts_Time_Covid_material/intermediate_data/replication_figures/comparison_global.xlsx")
+

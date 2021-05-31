cb9a76314bcbda70ab778ca3aa50a1601eb29510
Umberto Collodel <umberto.collodel@gmail.com>
2021-05-31 11:42:42 +0100

    finished cleaning global weights construction

--- 1.3 prepare_weights_aggregates.R
+++ 1.3 prepare_weights_aggregates.R
@@ -1,13 +1,10 @@
-########## Script to construct weights used in aggregate forecasts from WEO over different 2020 issues:
+########## Script to construct weights used in global forecasts for 2020 from WEO over different 2020 issues:
 
 # Set parameters: ----
 
 
 sheets=c("jan","apr","jun","oct") %>% 
   map_chr(~ paste0(.x,"2020"))
-
-paths=rep("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx",4)
-
 
 
 # Construct country weights for global aggregate from GDP PPP serie: ----
@@ -16,37 +13,25 @@ paths=rep("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx",4)
 
 
 df_weights_global <- sheets %>% 
-  map(~ read_xlsx("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx", sheet = .x)) %>%
-  map(~ .x %>% slice(1: which(Country == "Zimbabwe"))) %>% 
-  map(~ .x %>% select(Series_code,`1950`:ncol(.))) %>%
-  map(~ .x %>% mutate(Series_code = str_extract(Series_code, "\\d{3}"))) %>% 
-  map(~ .x %>% select(-matches("Q|M"))) %>% 
-  map(~ .x %>% gather("year","value",`1950`:ncol(.))) %>% 
-  map(~ .x %>% filter(year == 2020)) %>% 
-  map(~ .x %>% rename(country_code = Series_code)) %>% 
+  map(~ clean_weo_2020_forecasts("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx",sheet = .x)) %>% 
   map(~ .x %>% mutate(weight = value/sum(value,na.rm = T))) %>% 
   map2(sheets, ~ .x %>% mutate(horizon = str_to_sentence(str_remove(.y,"2020")))) %>%
   bind_rows() %>% 
-  select(-value)
+  select(-value) %>% 
+  suppressWarnings()
 
 
-# Actual value:
+# Actual value (April 2020 WEO issue): 
 
-df_weights_global_actual <- read_xlsx("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx", sheet = "apr2021") %>% 
-  slice(1: which(Country == "Zimbabwe")) %>% 
-  select(Series_code,`1950`:ncol(.)) %>%
-  mutate(Series_code = str_extract(Series_code, "\\d{3}")) %>% 
-  select(-matches("Q|M")) %>% 
-  gather("year","value",`1950`:ncol(.)) %>% 
-  filter(year == 2020) %>% 
-  rename(country_code = Series_code) %>% 
+df_weights_global_actual <- clean_weo_2020_forecasts("../Forecasts_Time_Covid_material/raw_data/gdp_ppp_2020.xlsx", sheet = "apr2021") %>% 
   mutate(weight = value/sum(value,na.rm = T)) %>%
   select(country_code,weight) %>% 
-  rename(actual_weight = weight)
+  rename(actual_weight = weight) %>% 
+  suppressWarnings()
   
   
 
 # Export: ----
 
 list(df_weights_global, df_weights_global_actual) %>% 
-  walk2(c("","_actual"), ~ saveRDS(.x,paste0("../Forecasts_Time_Covid_material/intermediate_data/weights_global",.y,".RDS")))
+  walk2(c("","_actual"), ~ saveRDS(.x,paste0("../Forecasts_Time_Covid_material/intermediate_data/weights_aggregates/weights_global",.y,".RDS")))
